a124fbb17ff9a42e8c26c36ee46d55defdf5bd25
Patrick Stuart <patrick@patrickstuart.com>
2017-03-03 09:57:04 -0600

    dashboards cols view templates.js added function to survive page reload for save and delete

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,5 +1,46 @@
  var editor = CodeMirror.fromTextArea(document.getElementById("templateEditor"), { lineNumbers: true, lineWrapping: true });
  editor.setSize(800, 500);
+ if (JSON && JSON.stringify && JSON.parse) var Session = Session || (function() {
+
+     // window object
+     var win = window.top || window;
+
+     // session store
+     var store = (win.name ? JSON.parse(win.name) : {});
+
+     // save store on page unload
+     function Save() {
+         win.name = JSON.stringify(store);
+     };
+
+     // page unload event
+     if (window.addEventListener) window.addEventListener("unload", Save, false);
+     else if (window.attachEvent) window.attachEvent("onunload", Save);
+     else window.onunload = Save;
+
+     // public methods
+     return {
+
+         // set a session variable
+         set: function(name, value) {
+             store[name] = value;
+         },
+
+         // get a session value
+         get: function(name) {
+             return (store[name] ? store[name] : undefined);
+         },
+
+         // clear session
+         clear: function() { store = {}; },
+
+         // dump session data
+         dump: function() { return JSON.stringify(store); }
+
+     };
+
+ })();
+
  $(function() {
      $("#save").on('click', function(e) {
          var id = $("#id").val();
@@ -17,7 +58,12 @@
                  complete: function(data) {
                      // do something
                      $('#results').text("Successfully Saved");
-                     setInterval(function() { location.reload() }, 1000);
+                     //setInterval(function() { location.reload() }, 1000);
+                     Session.set("result", "Successfully Saved");
+                     Session.set("tempId", id);
+                     //setInterval(function () {
+                     location.reload();
+                     //}, 2000);
                  }
              });
          }
@@ -28,6 +74,8 @@
          var id = $("#tempMenu").find("li").length;
          $("#id").val(id);
          var name = $("#name").val("");
+         //var content = editor.getDoc().getValue();
+         editor.getDoc().setValue("");
          var content = $('#templateEditor').val("");
      });
 
@@ -42,7 +90,11 @@
              complete: function(data) {
                  // do something
                  $('#results').text("Successfully Deleted");
-                 setInterval(function() { location.reload() }, 5000);
+                 var results = "Successfully Deleted";
+                 Session.set(result, "Successfully Deleted");
+                 //setInterval(function () {
+                 location.reload()
+                     //}, 2000);
              }
          });
      });
@@ -70,5 +122,23 @@
  $(document).ready(function() {
      var id = $("#tempMenu").find("li").length;
      $("#id").val(id);
-
+     if (Session.get("result") != "") {
+         $('#results').text(Session.get("result"));
+         Session.set("result", "");
+     }
+     if (Session.get("tempId") && Session.get("tempId") != "") {
+         $.ajax({
+             type: 'GET',
+             url: '/api/templates/' + Session.get("tempId") + '',
+             //dataType: 'json',
+             //data: JSON.stringify(emptyDevice),
+             //contentType: 'application/json',
+             complete: function(data) {
+                 //$('#templateEditor').val(data.responseText);
+                 editor.getDoc().setValue(data.responseText);
+                 $('#name').val(name);
+                 $('#id').val(id);
+             }
+         });
+     }
  });
