f678ac67f656a3ea407515512fc878f0109bc941
Patrick Stuart <patrick@patrickstuart.com>
2017-03-05 16:10:09 -0600

    added fullscreen toggle main menu

--- public/js/global.js
+++ public/js/global.js
@@ -1,14 +1,91 @@
+(function() {
+    var
+        fullScreenApi = {
+            supportsFullScreen: false,
+            isFullScreen: function() { return false; },
+            requestFullScreen: function() {},
+            cancelFullScreen: function() {},
+            fullScreenEventName: '',
+            prefix: ''
+        },
+        browserPrefixes = 'webkit moz o ms khtml'.split(' ');
+
+    // check for native support
+    if (typeof document.cancelFullScreen != 'undefined') {
+        fullScreenApi.supportsFullScreen = true;
+    } else {
+        // check for fullscreen support by vendor prefix
+        for (var i = 0, il = browserPrefixes.length; i < il; i++) {
+            fullScreenApi.prefix = browserPrefixes[i];
+
+            if (typeof document[fullScreenApi.prefix + 'CancelFullScreen'] != 'undefined') {
+                fullScreenApi.supportsFullScreen = true;
+
+                break;
+            }
+        }
+    }
+
+    // update methods to do something useful
+    if (fullScreenApi.supportsFullScreen) {
+        fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
+
+        fullScreenApi.isFullScreen = function() {
+            switch (this.prefix) {
+                case '':
+                    return document.fullScreen;
+                case 'webkit':
+                    return document.webkitIsFullScreen;
+                default:
+                    return document[this.prefix + 'FullScreen'];
+            }
+        }
+        fullScreenApi.requestFullScreen = function(el) {
+            return (this.prefix === '') ? el.requestFullScreen() : el[this.prefix + 'RequestFullScreen']();
+        }
+        fullScreenApi.cancelFullScreen = function(el) {
+            return (this.prefix === '') ? document.cancelFullScreen() : document[this.prefix + 'CancelFullScreen']();
+        }
+    }
+
+    // jQuery plugin
+    if (typeof jQuery != 'undefined') {
+        jQuery.fn.requestFullScreen = function() {
+
+            return this.each(function() {
+                if (fullScreenApi.supportsFullScreen) {
+                    fullScreenApi.requestFullScreen(this);
+                }
+            });
+        };
+    }
+
+    // export api
+    window.fullScreenApi = fullScreenApi;
+})();
+
 $(document).ready(function() {
     $('#right-charm .button').on('click', function(e) {
         //alert('button clicked');
-        var url = "";
-        if ($(this).attr('href')) {
-            url = $(this).attr('href');
+        if (e.target.textContent == "Toggle Fullscreen") {
+            var wrapper = document.getElementById('body');
+            if (!fullScreenApi.isFullScreen()) {
+                if (fullScreenApi.supportsFullScreen) {
+                    window.fullScreenApi.requestFullScreen(wrapper);
+                }
+            } else {
+                fullScreenApi.cancelFullScreen(wrapper);
+            }
         } else {
-            url = $(this).find('a').attr('href');
+            var url = "";
+            if ($(this).attr('href')) {
+                url = $(this).attr('href');
+            } else {
+                url = $(this).find('a').attr('href');
+            }
+            e.preventDefault();
+            window.location = url
         }
-        e.preventDefault();
-        window.location = url
     });
 
 });
