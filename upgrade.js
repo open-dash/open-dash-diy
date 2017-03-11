35d3782fadc52d6b07da7c0a9a35f09a41920ee7
Patrick Stuart <patrick@patrickstuart.com>
2017-03-11 11:32:46 -0600

    added upgrade function

---
+++ upgrade.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+console.log("backing up data/smartthings.json")
+fs.writeFileSync('data/dashboards.bak', fs.readFileSync('data/dashboards.json'));
+var obj = JSON.parse(fs.readFileSync('data/dashboards.json', 'utf8'));
+for (var x in obj.dashboards) {
+    var devices = obj.dashboards[x].devices;
+    devices.forEach(d => {
+        if (!d.api) {
+            d.api = "smartthings";
+        }
+    })
+}
+
+fs.writeFile('data/dashboards.json', JSON.stringify(obj), function(err) {
+    if (err) return console.error(err);
+    console.log('done upgrading');
+})
